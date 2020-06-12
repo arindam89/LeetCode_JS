2387ebf00f2b703a8af8953032da5089f866eb55
Arindam Paul <arindampaul1989@gmail.com>
2020-06-12 23:02:32 +0530

    Adding VSCode stuff as well.

--- strings/LC20_isValid/index.js
+++ strings/LC20_isValid/index.js
@@ -1,6 +1,26 @@
 // This is an exclusive problem only available in the Deluxe version of this course.
 // To learn more, visit https://kaeducation.com/lc-js.html
 
-function isValid(s) {}
+function isValid(s) {
+  let stack = [];
+  let top = 0;
+  let open_chars = ["(", "[", "{"];
+  let close_chars = [")", "]", "}"];
+  if (s.length === 1) return false;
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    const open_index = open_chars.indexOf(char);
+    if (open_index != -1) {
+      stack.push(char);
+    } else {
+      const topElement = stack[stack.length - 1];
+      if (open_chars.indexOf(topElement) === close_chars.indexOf(char)) {
+        stack.pop();
+      }
+    }
+  }
+  return stack.length === 0;
+}
 
 module.exports = isValid;
--- strings/LC3_lengthOfLongestSubstring/index.js
+++ strings/LC3_lengthOfLongestSubstring/index.js
@@ -1,3 +1,17 @@
-function lengthOfLongestSubstring(s) {}
+// Implementation by sliding window protocol.
+function lengthOfLongestSubstring(s) {
+  let startWindow = 0;
+  let startMap = {};
+  let maxWindow = 0;
+  for (let i = 0; i < s.length; i++) {
+    const endChar = s[i];
+    if (startMap[endChar] != undefined && startMap[endChar] >= startWindow) {
+      startWindow = startMap[endChar] + 1;
+    }
+    startMap[endChar] = i;
+    maxWindow = Math.max(maxWindow, i - startWindow + 1);
+  }
+  return maxWindow;
+}
 
 module.exports = lengthOfLongestSubstring;

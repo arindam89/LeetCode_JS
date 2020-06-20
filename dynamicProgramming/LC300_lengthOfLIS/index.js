84f8ba7c199859879395733c5b929a1f6a2206bb
Arindam Paul <arindampaul1989@gmail.com>
2020-06-20 22:59:06 +0530

    Added LC300 Solution as well.

--- dynamicProgramming/LC300_lengthOfLIS/index.js
+++ dynamicProgramming/LC300_lengthOfLIS/index.js
@@ -1,6 +1,37 @@
 // This is an exclusive problem only available in the Deluxe version of this course.
 // To learn more, visit https://kaeducation.com/lc-js.html
 
-function lengthOfLIS(nums) {}
+function lengthOfLIS(nums) {
+  // 1. If the number of elements less than 1 return length
+  if (nums && nums.length <= 1) return nums.length;
+
+  // 2. We create our DP array which holds the best subsequence
+  //    Initialize it with 1
+  //    Also have i and j which will be used to traverse the array
+  //    i -> represents from current index we are looking at the array
+  //    j -> represents the end index which we are considering (array length)
+  const sub = new Array(nums.length).fill(1);
+  let globalMax = 1;
+
+  // 3. Loop through the array with j starts at 1 index all the way
+  //    Have a inner loop with i which starts at 0 goes till j
+  //    Using the sub array look and update the sequence length
+  for (let j = 1; j < nums.length; j++) {
+    for (let i = 0; i < j; i++) {
+      // We found an element from which nums[j]
+      // can be a increasing subsequence
+      if (nums[j] > nums[i]) {
+        // We update sub at j if our current evalutation is better
+        // than what we already knew
+        sub[j] = Math.max(sub[i] + 1, sub[j]);
+        // update the globalMax subsequence as well if needed
+        if (sub[j] > globalMax) {
+          globalMax = sub[j];
+        }
+      }
+    }
+  }
+  return globalMax;
+}
 
 module.exports = lengthOfLIS;
--- dynamicProgramming/LC62_uniquePaths2/index.js
+++ dynamicProgramming/LC62_uniquePaths2/index.js
@@ -1,3 +1,20 @@
+/**
+ * 55. Jump Game
+ * =============
+ *
+ * Given an array of non-negative integers, you are initially positioned at the first index of the array.
+ * Each element in the array represents your maximum jump length at that position.
+ * Determine if you are able to reach the last index.
+ *
+ * Example 1:
+ *     Input: nums = [2,3,1,1,4]
+ *     Output: true
+ *     Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+ *
+ *
+ * @param {Array} obstacleGrid This is the obstacle grid.
+ */
+
 function uniquePathsWithObstacles(obstacleGrid) {
   const n = obstacleGrid.length;
   const m = obstacleGrid[0].length;
